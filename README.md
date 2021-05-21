1632c6a6724a739f8dd7a82d3dcd23c8e1b9c129
Ed Blin <edouard.blin@alterassets.io>
2021-05-21 23:25:57 +0200

    setup app and home page

---
+++ README.md
@@ -0,0 +1,8 @@
+To run locally:
+1. from within the project folder:
+    `python3 -m venv venv`
+2. then type in your terminal:
+    `. venv/bin/activate`
+3. finally:
+    `export FLASK_APP=application`
+
---
+++ application.py
@@ -0,0 +1,24 @@
+from flask import Flask, flash, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # To DO ALGORITHM
+        print("todo")
+    else:
+        return render_template("index.html")
